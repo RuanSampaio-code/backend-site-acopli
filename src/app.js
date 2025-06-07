2fc483fd8f38eeb2232576c1e8b6337828be9666
RuanSampaio-code <ruansampaio001@gmail.com>
2025-06-07 00:07:34 -0300

    adcionando cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import routes from './routes/index.js';
+import cors from 'cors';
+
 
 const app = express();
+
+
+// Ativa o uso do CORS
+app.use(cors()); // permite todas as origens (pode restringir depois se quiser)
 
 routes(app);
 
